12f18c17b6004520a25021eeed7caca8868dd676
Проць Іван <ivanprotss@gmail.com>
2021-05-20 17:38:22 +0300

    started project

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import {render} from 'react-dom';
+import Landing from "./components/landing"
+
+render(<Landing />, document.querySelector('#root'));
---
+++ src/sample-restaurants.js
@@ -0,0 +1,20 @@
+const restaurants = [
+  {
+    title: 'Кебаб Шеф Січових',
+    url: 'kebab-shef-sichovih',
+    id: 1
+  },
+
+  {
+    title: 'Кебаб Шеф Бандери',
+    url: 'kebab-shef-bandery',
+    id: 2
+  },
+  {
+    title: 'Кебаб Шеф Валова',
+    url: 'kebab-shef-valova',
+    id: 3
+  }
+];
+
+export default restaurants;
