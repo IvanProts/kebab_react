fa96dafb1e35eb2c36cf4ffef8388df5e40761a8
Проць Іван <ivanprotss@gmail.com>
2021-05-27 12:02:03 +0300

    added some functional

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import Header from './Header';
 import Order from './Order';
+import Burger from './Burger';
 import MenuAdmin from './MenuAdmin';
+import sampleBurgers from '../sample-burgers';
 
 class App extends React.Component {
 
@@ -16,14 +18,41 @@ class App extends React.Component {
     this.setState({burgers});
   }
 
+  addToOrder = (key) => {
+    const order = {...this.state.order};
+    order[key] = order[key] + 1 || 1;
+    this.setState({ order });
+  }
+
+  loadSampleBurgers = () => {
+    this.setState({
+      burgers: sampleBurgers
+    });
+  }
+
   render(){
     return(
       <div className="burger-paradise">
         <div className="menu">
           <Header title="Дуже сочні кебабусики"/>
+          <ul className="burgers">
+            {Object.keys(this.state.burgers).map(key => {
+              return (
+              <Burger 
+              key={key}
+              index={key}
+              addToOrder={this.addToOrder}
+              details={this.state.burgers[key]}
+              />
+              );
+            })}
+          </ul>
         </div>
           <Order/>
-          <MenuAdmin addBurger={this.addBurger}/>
+          <MenuAdmin addBurger={this.addBurger}
+          loadSampleBurgers = {
+            this.loadSampleBurgers}
+          />
       </div>
     )
   }
--- src/components/MenuAdmin.js
+++ src/components/MenuAdmin.js
@@ -7,6 +7,7 @@ class MenuAdmin extends React.Component {
       <div className="menu-admin">
         <h2>Керування Меню</h2>
         <AddBurgerForm addBurger={this.props.addBurger}/>
+        <button onClick={this.props.loadSampleBurgers}>Загрузити бургери</button>
       </div>
     )
   }
